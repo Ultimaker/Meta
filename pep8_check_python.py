79ef5b1aee7e9f7dfc3af62f130a540b9dd5d21a
daid <daid303@gmail.com>
2015-10-27 09:25:45 +0100

    Update on pep8 check script to allow for xml output. Which can be used for jenkins as test results.

--- pep8_check_python.py
+++ pep8_check_python.py
@@ -5,7 +5,9 @@ import pep8
 import re
 import os
 import sys
+import argparse
 import token as token_type
+from xml.etree import ElementTree
 
 CLASS_NAME_MATCH = re.compile(r'^_*[A-Z][a-zA-Z0-9]*_*$')
 FUNCTION_NAME_MATCH = re.compile(r'^(_*|test_)[a-z][a-zA-Z0-9]*_*$')
@@ -19,7 +21,7 @@ indent_stack = []
 # The coding style set at Ultimaker.
 def checkNames(logical_line, physical_line, tokens, indent_level):
     global indent_stack
-    if logical_line != '':
+    if logical_line != "":
         while indent_stack and indent_stack[-1][0] >= indent_level:
             indent_stack.pop()
         if not indent_stack or indent_stack[-1][0] < indent_level:
@@ -67,9 +69,9 @@ def checkNames(logical_line, physical_line, tokens, indent_level):
         if not MEMBER_NAME_MATCH.match(tokens[idx + 2].string):
             yield tokens[idx + 2].start, "U202 Member name not in lower_case_underscore_format"
     elif len(tokens) > idx + 2 and tokens[idx + 1].string == "=":
-        if len(indent_stack) > 1 and indent_stack[-2][1].startswith('class '):
+        if len(indent_stack) > 1 and indent_stack[-2][1].startswith("class "):
             # definition is a class member
-            if tokens[idx].string.startswith('_'):
+            if tokens[idx].string.startswith("_"):
                 # class member is a private, match the member name style.
                 if not MEMBER_NAME_MATCH.match(tokens[idx].string):
                     yield tokens[idx].start, "U202 Member name not in lower_case_underscore_format"
@@ -99,14 +101,54 @@ def blankLines(logical_line, blank_lines, indent_level, line_number, blank_befor
             yield 0, "U302 expected 2 blank lines, found 0"
 
 
-def main(paths=["."]):
+class XmlReport(pep8.StandardReport):
+    def __init__(self, options):
+        super().__init__(options)
+        self._error_files = {}
+
+    def error(self, line_number, offset, text, check):
+        super().error(line_number, offset, text, check)
+
+        code = text[:4]
+        if self._ignore_code(code):
+            return
+        
+        if self.filename not in self._error_files:
+            self._error_files[self.filename] = []
+        lines = self.lines[line_number-2:line_number+2]
+        lines.insert(2, (" " * offset) + "^\n")
+        self._error_files[self.filename].append((line_number, text, lines))
+    
+    def getJUnitXml(self):
+        xml = ElementTree.Element("testsuites")
+        xml.text = "\n"
+        for filename, data in self._error_files.items():
+            testsuite = ElementTree.SubElement(xml, "testsuite", {"name": filename, "errors": "0", "tests": str(len(data)), "failures": str(len(data)), "time": "0", "timestamp":"2013-05-24T10:23:58"})
+            testsuite.text = "\n"
+            testsuite.tail = "\n"
+            for line_number, text, lines in data:
+                testcase = ElementTree.SubElement(testsuite, "testcase", {"classname": "%s.line_%d" % (filename, line_number), "name": text})
+                testcase.text = "\n"
+                testcase.tail = "\n"
+                failure = ElementTree.SubElement(testcase, "failure", {"message": "test failure"})
+                failure.text = "".join(lines)
+                failure.tail = "\n"
+        return ElementTree.ElementTree(xml)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="pep8 Ultimaker style checker")
+    parser.add_argument("paths", type=str, nargs="*", help="List of paths to check (files or directories)", default=["."])
+    parser.add_argument("--xml", type=str, help="Output JUnit XML file")
+    args = parser.parse_args()
+
     pep8.register_check(checkNames)
     pep8.register_check(blankLines)
     pep8.register_check(checkStrings)
 
     ignore = []
     ignore.append("E501")  # Ignore line length violations.
-    ignore.append("E266")  # Ignore too many leading # in comment block.
+    ignore.append("E226")  # Ignore too many leading # in comment block.
 
     critical = []
     critical.append("E301")  # expected 1 blank line, found 0
@@ -120,7 +162,9 @@ def main(paths=["."]):
     critical.append("U")     # All Ultimaker specific stuff.
 
     pep8style = pep8.StyleGuide(quiet=False, select=critical, ignore=ignore, show_source=True)
-    for path in paths:
+    if args.xml is not None:
+        pep8style.init_report(XmlReport)
+    for path in args.paths:
         if path.endswith(".py") and "_pb2.py" not in path:
             pep8style.paths.append(path)
         for base_path, _, filenames in os.walk(path):
@@ -132,8 +176,8 @@ def main(paths=["."]):
     result.print_statistics()
     print("Total: %d" % (result.get_count()))
 
+    if args.xml is not None:
+        result.getJUnitXml().write(args.xml, "utf-8", True)
+
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        main(sys.argv[1:])
-    else:
-        main()
+    main()

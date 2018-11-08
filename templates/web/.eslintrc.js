78c6c5c23759a59c797dc7cb819e91b304de409c
Ian Paschal <projects@ianpaschal.com>
2018-11-08 15:33:36 +0100

    Improve .eslintrc.js
    Contributes to SA-23

--- templates/web/.eslintrc.js
+++ templates/web/.eslintrc.js
@@ -16,6 +16,9 @@ module.exports = {
     plugins: ["import"],
     rules: {
         "indent": ["error", 4, {
+
+            // These overrides are required to keep React code looking good
+            // They don't affect non-React code
             "ignoredNodes": [
                 "JSXElement",
                 "JSXElement > *",
@@ -33,23 +36,31 @@ module.exports = {
             ]
         }],
         "no-underscore-dangle": 0,
-        "import/no-unresolved": 0
+        "import/no-unresolved": 0,
+        "jsx-closing-tag-location": 0
     },
     settings: {
         "import/extensions": [".js", ".jsx"]
     },
+
+    // Create some special overrides for TypeScript files
     overrides: [
         {
             files: ["**/*.ts", "**/*.tsx"],
             parser: "typescript-eslint-parser",
             rules: {
+
+                // Required for interfaces
                 "no-undef": 0,
+
+                // Use .tsx instead of .jsx
                 "react/jsx-filename-extension": [1, {
                     "extensions": [".tsx"]
-                }],
-                "jsx-closing-tag-location": 0
+                }]
             },
             settings: {
+
+                // Allow import of .ts and .tsx without specifying extension
                 "import/extensions": [".ts", ".tsx"]
             }
         }
